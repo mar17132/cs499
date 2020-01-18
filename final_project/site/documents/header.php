3842026483d39c0da8943bf41243e8c0e8c6efed
Matthew Martin <mar17132@byui.edu>
2020-01-18 13:17:08 -0700

    update

--- final_project/site/documents/header.php
+++ final_project/site/documents/header.php
@@ -1,7 +1,7 @@
 
+
+
 <?php
-
-
 
 
 ?>
@@ -14,11 +14,14 @@
     <head>
 	<meta charset="utf-8" />
         <title>CS Final</title>
-        <link rel="stylesheet" href="scripts/bootstrap/4.4.1/css/bootstrap.min.css"/>
-        <script src="scripts/jquery-3.4.1.min.js" ></script>
-        <script src="scripts/bootstrap/4.4.1/js/bootstrap.bundle.min.js" ></script>
+        <link rel="stylesheet" href="../scripts/bootstrap/4.4.1/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../styles/main_menu.css"/>
+        <script type="text/javascript" src="../scripts/jquery-3.4.1.min.js" ></script>
+        <script type="text/javascript" src="../scripts/bootstrap/4.4.1/js/bootstrap.bundle.min.js" ></script>
         <style type="text/css">
         </style>    
     </head>
     <body>
+
+    <?php include_once 'menu.php'; ?>
 
---
+++ final_project/site/documents/menu.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+?>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" 
+  data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" 
+  aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">
+          Home 
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Studys
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Interviews
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Populations
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          Users
+        </a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav ml-auto">
+      <li>
+        <a class="nav-link"  id="menu_logout">
+            Logout
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
