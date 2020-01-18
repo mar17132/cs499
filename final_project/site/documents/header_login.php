1e4bc394d6c21e76457026327ad0bccc93fc4a10
Matthew Martin <mar17132@byui.edu>
2020-01-18 15:43:53 -0700

    login

---
+++ final_project/site/documents/header_login.php
@@ -0,0 +1,25 @@
+
+
+
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE HTML>
+
+<html lang="en" >
+    <head>
+	<meta charset="utf-8" />
+        <title>CS Final</title>
+        <link rel="stylesheet" href="../scripts/bootstrap/4.4.1/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="../styles/login.css"/>
+        <script type="text/javascript" src="../scripts/jquery-3.4.1.min.js" ></script>
+        <script type="text/javascript" src="../scripts/bootstrap/4.4.1/js/bootstrap.bundle.min.js" ></script>
+        <style type="text/css">
+        </style>    
+    </head>
+    <body>
+
