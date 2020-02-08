6231b5d79274c337aee9c912fd1d003fabb7abed
Matthew Martin <mar17132@byui.edu>
2020-02-08 11:15:04 -0700

    pop update

--- final_project/site/documents/header_index.php
+++ final_project/site/documents/header_index.php
@@ -17,7 +17,7 @@ require_once 'scripts/sessions.php';
         <link rel="stylesheet" href="scripts/bootstrap/4.4.1/css/bootstrap.min.css"/>
         <link rel="stylesheet" href="styles/body.css"/>
         <link rel="stylesheet" href="styles/main_menu.css"/>
-        <link rel="stylesheet" href="styles/page_body.css"/>
+        <link rel="stylesheet" href="styles/user_body.css"/>
         <script type="text/javascript" src="scripts/jquery-3.4.1.min.js" ></script>
         <script type="text/javascript" src="scripts/bootstrap/4.4.1/js/bootstrap.bundle.min.js" ></script>
         <style type="text/css">
--- final_project/site/documents/population_body.php
+++ final_project/site/documents/population_body.php
@@ -1,7 +1,45 @@
-<?php
+
+<?php require_once 'header.php'; ?>
 
 
 
 
-?>
+<nav class="navbar navbar-expand-sm bg-light justify-content-center page-title">
+    <h3 class="justify-content-center page-name">Population</h3>
+</nav>
+
+<div class="user-table container">
+<nav class="navbar navbar-expand-sm bg-dark pop-cmd-bar">
+    <button type="button" class="btn btn-secondary btn-sm pop-cmd-btn"
+            id="add-pop-btn" >
+        Add Person
+    </button>
+    <button type="button" class="btn btn-secondary btn-sm pop-cmd-btn"
+            id="view-Groups-btn" >
+        Groups
+    </button>
+    <button type="button" class="btn btn-secondary btn-sm pop-cmd-btn"
+            id="add-Groups-btn" >
+        Add Groups
+    </button>
+</nav>
+     <?php require_once 'partial/population_table.php';  ?>
+</div>
+
+<?php include_once 'partial/population_group_modal.php'; ?>
+
+<?php include_once 'partial/pop_add_edit_modal.php'; ?>
+
+<?php include_once 'partial/pop_delete_modal.php'; ?>
+
+<?php include_once 'partial/information_modal.php'; ?>
+
+<?php include_once 'partial/error_modal.php'; ?>
+
+<script type="text/javascript" src="../scripts/get_api_data.js" ></script>  
+<script type="text/javascript" src="../scripts/pop_body.js" ></script>    
+
+
+<?php include_once 'footer.php'; ?>
+
 
